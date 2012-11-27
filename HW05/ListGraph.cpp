04afa283d9a0151af1a4205e74ce8fb8d1cf59e8
Jake Gregg <greggjs@muohio.edu>
2012-11-27 10:23:59 -0500

    Made MatrixGraph.cpp and ListGraph.cpp
    Created cpp files and added all methods, not implemented yet though

---
+++ HW05/ListGraph.cpp
@@ -0,0 +1,38 @@
+//
+//	ListGraph.cpp
+//
+//	Created by Jake Gregg 11/27/2012
+//
+
+#include "ListGraph.h"
+
+using namespace std;
+
+ListGraph::ListGraph(int num_nodes) {
+
+}
+
+ListGraph::~ListGraph() {
+
+}
+
+EdgeWeight ListGraph::weight(NodeID u, NodeID v) const {
+
+}
+
+list<NWPair> ListGraph::getAdj(NodeID u) const {
+
+}
+
+unsigned ListGraph::degree(NodeID u) const {
+
+}
+
+unsigned ListGraph::size() const {
+
+}
+
+unsigned ListGraph::numEdges() const {
+
+}
+
---
+++ HW05/MatrixGraph.cpp
@@ -0,0 +1,41 @@
+//
+//	MatrixGraph.cpp
+//
+//	Create by Jake Gregg 11/26/2012
+//
+
+#include "MatrixGraph.h"
+
+using namespace std;
+
+MatrixGraph::MatrixGraph(unsigned int num_nodes) {
+	
+}
+
+MatrixGraph::~MatrixGraph() {
+
+}
+
+void MatrixGraph::addEdge(NodeID u, NodeID v, EdgeWeight weight) {
+
+}
+
+EdgeWeight MatrixGraph::weight(NodeID u, NodeID v) const {
+
+}
+
+list<NWPair> MatrixGraph::getAdj(NodeID u) const {
+
+}
+
+unsigned MatrixGraph::degree(NodeID u) const {
+
+}
+
+unsigned MatrixGraph::size() const {
+
+}
+
+unsigned MatrixGraph::numEdges() const {
+
+}
